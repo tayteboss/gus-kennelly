cc1da98bbbc887a7d28f6ea6575643c9dd1519e9
Tayte Boss <boss.tayte@gmail.com>
2023-09-21 09:33:46 +0700

    [feat] 404

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,15 +1,31 @@
 import { NextSeo } from 'next-seo';
 import styled from 'styled-components';
+import LayoutWrapper from '../components/common/LayoutWrapper';
+import Link from 'next/link';
+import pxToRem from '../utils/pxToRem';
 
-const PageWrapper = styled.div``;
+const PageWrapper = styled.div`
+	padding-top: ${pxToRem(16)};
+`;
+
+const Title = styled.h1`
+	margin-bottom: ${pxToRem(8)};
+`;
+
+const LinkTag = styled.a``;
 
 const Page = () => {
 	return (
 		<PageWrapper>
 			<NextSeo
-				title="Boiler"
-				description="Boiler Plate"
+				title="Gus Kennelly - 404"
 			/>
+			<LayoutWrapper>
+				<Title>Sorry, we couldn't find that page.</Title>
+				<Link href="/" passHref>
+					<LinkTag>Back home</LinkTag>
+				</Link>
+			</LayoutWrapper>
 		</PageWrapper>
 	)
 }
