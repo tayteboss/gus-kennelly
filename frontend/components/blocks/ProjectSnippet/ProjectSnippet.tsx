9d33ee44af15af79fc6f76c06c0d4082e0084b22
Tayte Boss <boss.tayte@gmail.com>
2023-09-24 14:13:57 +0700

    [feat] Testing mobile first time play

--- frontend/components/blocks/ProjectSnippet/ProjectSnippet.tsx
+++ frontend/components/blocks/ProjectSnippet/ProjectSnippet.tsx
@@ -124,7 +124,7 @@ const ProjectSnippet = (props: Props) => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [isHovered, setIsHovered] = useState(false);
 	const [isMuted, setIsMuted] = useState(true);
-	const [isPlaying, setIsPlaying] = useState(false);
+	const [isPlaying, setIsPlaying] = useState(true);
 	const [ratioHeight, setRatioHeight] = useState(350);
 	const [currentTime, setCurrentTime] = useState(0);
 	const [videoLength, setVideoLength] = useState(snippetData?.media?.asset?.data?.duration);
